4a1bc98dbafbcfecb545f05e80bff01b6eaad026
Evangelos Konstantinidis <Evangelos.Konstantinidis@EXXETA.com>
2018-02-27 09:56:35 +0100

    Finished project

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Konstantinidis, Evangelos on 27.02.18.
+//  Copyright © 2018 Konstantinidis, Evangelos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
